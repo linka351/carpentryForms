32b522c3ed57eb7cdb86964331a5b9fd6a6f7445
linka351 <linka351@gmail.com>
2025-10-08 18:28:04 +0200

    chore: initialize project with routing, state, and styling

---
+++ src/tests/test.test.tsx
@@ -0,0 +1,5 @@
+import { test } from "vitest";
+
+test("placeholder test", () => {
+  console.log("test");
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,4 @@
+import type { Config } from "tailwindcss";
+const config = {} satisfies Config;
+
+export default config;
